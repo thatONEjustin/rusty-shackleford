4ea3dd360bf718ba62727be235bb68c7b6bdb99d
thatONEjustin <justinpaelmo@gmail.com>
2022-11-03 21:25:47 -0500

    guessing game example complete!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,19 +8,36 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Secret Number {secret_number}");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
+    loop {
     
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num, 
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
+    
 }
